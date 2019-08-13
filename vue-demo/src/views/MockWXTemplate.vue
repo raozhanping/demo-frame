3072f970ac4f60991c61ca0a520a91e8a483c9bf
raozhanping <raozhanping@360.cn>
2019-08-13 15:21:42 +0800

    feat: add vue demo

---
+++ vue-demo/src/views/MockWXTemplate.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="mock-wx-template">
+    <template v-template name="customeName">
+      <view>自定义模板</view>
+      <span>自定义模板...</span>
+    </template>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+Vue.directive('template', {
+  
+})
+export default {
+  name: 'mock-wx-template',
+  components: {
+  }
+}
+</script>
+<style scoped>
+.mock-wx-template {
+  padding: 20px 15px 40px;
+}
+</style>
