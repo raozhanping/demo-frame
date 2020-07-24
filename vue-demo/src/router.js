2d945e0106016d9c5c89187c61e5c25ee91a8e94
zhanping <raozhanping@gmail.com>
2020-07-24 15:20:22 +0800

    feat: add vue-route

--- vue-demo/src/router.js
+++ vue-demo/src/router.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import Router from './plugins/vue-router'
 import Home from './views/Home.vue'
 import Example from './views/Example'
 import MockWXTemplate from './views/MockWXTemplate'
